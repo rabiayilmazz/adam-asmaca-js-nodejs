fb1b96e3511ed8eafe79ec3e9fce1dbd033e2d50
rabia yılmaz <rabiarabiabbb@gmail.com>
2021-08-05 16:20:46 +0300

    paketler yönetiliyor

--- intro.js
+++ intro.js
@@ -1,6 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 
+var express = require('express');
+
 http.createServer(function(req, res){
     fs.readFile('./index.html', function(err,data){
         if(err){
@@ -12,5 +14,17 @@ http.createServer(function(req, res){
         res.end()
 
     });
+
+    express.length('?harf=a', function(req,res){
+        console.log("okudu")
+    })
+
+    /*fs.readFile('./script.js',function(err,data){
+        if(err){
+            throw err;
+        }
+        res.write(data);
+        res.end();
+    });*/
     
 }).listen(8080)
